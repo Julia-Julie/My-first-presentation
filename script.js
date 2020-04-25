7bbdc1f525edeaeff9f0236985a99c6c4b85c092
Julia <julyaremko@gmail.com>
2020-04-25 14:52:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// jQuery [Scrolling]
+$(".btn").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+  
+      const hash = this.hash;
+      $("html, body").animate({ scrollTop: $(hash).offset().top}, 1100);
+    }
+  });
